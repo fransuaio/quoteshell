3dca4e18c80eefb7478eade58365406e19312539
Francisco Suárez <francisco.suarez.xyz@gmail.com>
2018-04-19 11:36:16 -0400

    add ';'

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const chalk = require('chalk')
+const chalk = require('chalk');
 
-const log = console.log
+const log = console.log;
 
 const axios = require('axios');
   axios('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback')
     .then((quote) => {
-      const quoteStringRaw = quote.data	
-      const quoteArrRaw = quote.data.split("")      
-	    const limit = quoteArrRaw.length - 1
-      let ac = ''
+      const quoteStringRaw = quote.data;	
+      const quoteArrRaw = quote.data.split("");      
+	    const limit = quoteArrRaw.length - 1;
+      let dataString = '';
       quoteArrRaw.map( (c, i) => {
   	    if (i >= 0 && i <= 15) {
 
@@ -19,25 +19,25 @@ const axios = require('axios');
         }       
       })
     
-      const quoteRaw = JSON.parse(ac)
-      const author = quoteRaw.title
-      const quoteTextRaw = quoteRaw.content.split("")
-      const phraseLength = quoteTextRaw.length - 1  
-      let phraseClean = ''
+      const quoteRaw = JSON.parse(dataString);
+      const author = quoteRaw.title;
+      const quoteTextRaw = quoteRaw.content.split("");
+      const phraseLength = quoteTextRaw.length - 1;
+      let phraseClean = '';
     
       quoteTextRaw.map((c, i) => {
         if (i <= 2) {
         } else if (i <= phraseLength - 6){          
-           phraseClean += c
+           phraseClean += c;
         }       
       })
       
-      log(chalk.cyan(phraseClean)+'\n \t \t -- '+chalk.bgBlackBright(author))    
+      log(chalk.cyan(phraseClean)+'\n \t \t -- '+chalk.bgBlackBright(author));
 
     })
 
     .catch( (error) => {
       if (error == 'Error: getaddrinfo EAI_AGAIN quotesondesign.com:80')
-      log(chalk.red('Please check your internet connection :('))  
+      log(chalk.red('Please check your internet connection :('));
     })
 
